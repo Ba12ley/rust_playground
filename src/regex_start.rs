36e5cd7f0c00d42961e765e26ccd8d39b06d0154
Dornar <59340405+Ba12ley@users.noreply.github.com>
2023-01-31 08:26:57 +0000

    regex

--- src/regex_start.rs
+++ src/regex_start.rs
@@ -1,1 +1,8 @@
-//comment
+extern crate regex;
+use regex::Regex;
+
+pub fn regex_start() {
+    let re = Regex::new(r"\w{5}").unwrap(); // \w is a word character, {5} is the number of characters
+    let word = "Words";
+    println!("Does {} match the regex? {}", word, re.is_match(word));
+}
